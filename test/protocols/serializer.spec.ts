8ebe3b9a172b2acf355ec8e2a33670267e886cd2
Dominik Schöni <dominik@dominikschoeni.ch>
2018-10-29 11:36:42 +0100

    feat(scheme): splitted scheme file

--- test/protocols/serializer.spec.ts
+++ test/protocols/serializer.spec.ts
@@ -1,10 +1,12 @@
-import * as sinon from 'sinon'
 import 'mocha'
 
 import { expect } from 'chai'
-import { UnsignedEthereumTransaction, EthereumUnsignedTransactionSerializer } from '../../lib/serializer/airgap-protocol-serializer'
 import { TestProtocolSpec } from './implementations'
 import { ethereumProtocol } from './specs/ethereum'
+import {
+  UnsignedEthereumTransaction,
+  EthereumUnsignedTransactionSerializer
+} from '../../lib/serializer/transactions/ethereum-transactions.serializer'
 
 const protocols = [ethereumProtocol]
 
@@ -12,7 +14,7 @@ protocols.forEach((protocol: TestProtocolSpec) => {
   describe(`AirGap Serialization Protocol`, () => {
     it('should be able to serialize an EthereumTx to a airgap protocol string', () => {
       const ethereumTx: UnsignedEthereumTransaction = ['0x00', '0x3b9aca00', '0x5208', protocol.wallet.address, '0x8ac7230489e80000', '0x']
-      const txSerializer = new EthereumUnsignedTransactionSerializer(ethereumProtocol.lib)
+      const txSerializer = new EthereumUnsignedTransactionSerializer()
       expect(
         txSerializer.serialize(
           protocol.wallet.address,
