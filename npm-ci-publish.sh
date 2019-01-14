238e4df328ab5465b98cd17596553365f57f3d4c
Dominik Schöni <dominik@dominikschoeni.ch>
2019-01-14 11:34:01 +0100

    fix(tezos): adapted bash-script to target @next if beta is detected

--- npm-ci-publish.sh
+++ npm-ci-publish.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > .npmrc
+
+$VERSION = "$(node -pe 'JSON.parse(process.argv[1]).version' \"$(cat package.json)\")"
+
+if [[ $VERSION == *"beta"* ]]; then
+  npm publish --tag next
+else
+  npm publish
+fi
+
 npm publish 
 rm .npmrc
