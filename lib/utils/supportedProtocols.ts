116b77105a17268ad8e97320ed93d6236ec3f08a
Andreas Gassmann <andreas@andreasgassmann.ch>
2019-02-20 11:10:12 +0100

    fix(): tests failing due to circular dependency

--- lib/utils/supportedProtocols.ts
+++ lib/utils/supportedProtocols.ts
@@ -4,14 +4,20 @@ import { EthereumProtocol } from '../protocols/ethereum/EthereumProtocol'
 import { AEProtocol } from '../protocols/aeternity/AEProtocol'
 import { TezosProtocol } from '../protocols/tezos/TezosProtocol'
 
-const protocols: ICoinProtocol[] = [new BitcoinProtocol(), new EthereumProtocol(), new AEProtocol(), new TezosProtocol()]
+const protocols: ICoinProtocol[] = []
 
 const supportedProtocols = function(): ICoinProtocol[] {
+  if (protocols.length === 0) {
+    // We cannot assign the protocols outside a function because the compiler complains
+    // The reason is that we have a circular dependency in the EthereumProtocol.
+    protocols.push(new BitcoinProtocol(), new EthereumProtocol(), new AEProtocol(), new TezosProtocol())
+  }
+
   return protocols
 }
 
 const addSupportedProtocol = function(newProtocol: ICoinProtocol): void {
-  if (protocols.find(protocol => protocol.identifier === newProtocol.identifier)) {
+  if (supportedProtocols().find(protocol => protocol.identifier === newProtocol.identifier)) {
     throw new Error(`protocol ${newProtocol.name} already exists`)
   }
 
