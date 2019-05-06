f0d2f44844679f2e670b359c34737fda7b080496
Andreas Gassmann <andreas@andreasgassmann.ch>
2019-05-06 11:05:23 +0200

    feat(grs): enable grs

--- lib/utils/supportedProtocols.ts
+++ lib/utils/supportedProtocols.ts
@@ -11,7 +11,7 @@ const supportedProtocols = function(): ICoinProtocol[] {
   if (protocols.length === 0) {
     // We cannot assign the protocols outside a function because the compiler complains
     // The reason is that we have a circular dependency in the EthereumProtocol.
-    protocols.push(new BitcoinProtocol(), /* new GroestlcoinProtocol(), */ new EthereumProtocol(), new AEProtocol(), new TezosProtocol())
+    protocols.push(new AEProtocol(), new BitcoinProtocol(), new EthereumProtocol(), new GroestlcoinProtocol(), new TezosProtocol())
   }
 
   return protocols
