134b367a7eef641a7c4f4b113d77fafabdbb23df
bs <bs@macrix.pl>
2019-05-30 07:34:08 +0200

    Preparing stubs for unit tests

--- test/protocols/stubs/xrp.stub.ts
+++ test/protocols/stubs/xrp.stub.ts
@@ -2,9 +2,25 @@ import { ProtocolHTTPStub, TestProtocolSpec } from '../implementations'
 import { XrpProtocol } from '../../../lib/protocols/xrp/XrpProtocol'
 import * as sinon from 'sinon'
 import BigNumber from 'bignumber.js'
+import { RippleAPI, FormattedTransactionType, RippleAPIBroadcast } from 'ripple-lib'
+import { FormattedGetAccountInfoResponse } from 'ripple-lib/dist/npm/ledger/accountinfo'
 
 export class XrpProtocolStub implements ProtocolHTTPStub {
   registerStub(testProtocolSpec: TestProtocolSpec, protocol: XrpProtocol) {
+    let accountInfo: FormattedGetAccountInfoResponse = {
+      ownerCount: 1,
+      previousAffectingTransactionID: '',
+      previousAffectingTransactionLedgerVersion: 1,
+      previousInitiatedTransactionID: '',
+      sequence: 0,
+      xrpBalance: '100000000'
+    }
+
+    const stubAcountInfo = sinon
+      .stub(RippleAPI, 'getAccountInfo')
+      .withArgs(sinon.match.any)
+      .returns(Promise.resolve(accountInfo))
+
     //sinon
     //    .stub(protocol.web3.eth, 'getTransactionCount')
     //    .withArgs(testProtocolSpec.wallet.addresses[0])
