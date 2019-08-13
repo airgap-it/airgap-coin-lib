cffdb1cc13189738f2f2f65ed578166265e597c3
Andreas Gassmann <andreas@andreasgassmann.ch>
2019-08-13 11:34:39 +0200

    fix(test): remove empty zcash protocol

--- src/protocols/zcash/ZCashTestnetProtocol.ts
+++
@@ -1,9 +0,0 @@
-import { BitcoinProtocol } from '../bitcoin/BitcoinProtocol'
-const zcashJS = require('bitcoinjs-lib-zcash')
-import { networks } from '../../networks'
-
-export class ZCashTestnetProtocol extends BitcoinProtocol {
-  constructor() {
-    super(networks.zcash, 'https://explorer.testnet.z.cash', zcashJS) // we probably need another network here, explorer is ok
-  }
-}
