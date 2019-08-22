2da2a5e48490f7b0e0bbc28fa28f9475dec79a02
Mike Godenzi <m.godenzi@papers.ch>
2019-08-22 15:59:57 +0200

    feat(web3-replacement): implemented nodeclient to replace web3

--- src/protocols/ethereum/EthereumProtocol.ts
+++ src/protocols/ethereum/EthereumProtocol.ts
@@ -1,3 +1,13 @@
 import { BaseEthereumProtocol } from './BaseEthereumProtocol'
+import { AirGapNodeClient } from './clients/node-clients/AirGapNodeClient'
+import { TrustWalletInfoClient } from './clients/info-clients/InfoClient'
 
-export class EthereumProtocol extends BaseEthereumProtocol {}
+export class EthereumProtocol extends BaseEthereumProtocol<AirGapNodeClient, TrustWalletInfoClient> {
+  constructor() {
+    super({
+      chainID: 1,
+      nodeClient: new AirGapNodeClient(),
+      infoClient: new TrustWalletInfoClient()
+    })
+  }
+}
--- src/protocols/ethereum/EthereumRopstenProtocol.ts
+++ src/protocols/ethereum/EthereumRopstenProtocol.ts
@@ -1,7 +1,14 @@
 import { BaseEthereumProtocol } from './BaseEthereumProtocol'
+import { AirGapNodeClient } from './clients/node-clients/AirGapNodeClient'
+import { TrustWalletInfoClient } from './clients/info-clients/InfoClient'
 
-export class EthereumRopstenProtocol extends BaseEthereumProtocol {
+export class EthereumRopstenProtocol extends BaseEthereumProtocol<AirGapNodeClient, TrustWalletInfoClient> {
   constructor() {
-    super('https://ropsten.infura.io/', 'https://ropsten.trustwalletapp.com', 3) // we probably need another network here, explorer is ok
+    // we probably need another network here, explorer is ok
+    super({
+      chainID: 3,
+      nodeClient: new AirGapNodeClient('https://ropsten.infura.io/'),
+      infoClient: new TrustWalletInfoClient('https://ropsten.trustwalletapp.com/')
+    })
   }
 }
