8a63018e86647923078eeb6226ca2602d834c8cb
Mike Godenzi <m.godenzi@papers.ch>
2019-08-26 10:35:25 +0200

    fix(web3-replacement): updated tests to remove references to web3

--- test/protocols/stubs/ethereum.stub.ts
+++ test/protocols/stubs/ethereum.stub.ts
@@ -7,11 +7,11 @@ import { ProtocolHTTPStub, TestProtocolSpec } from '../implementations'
 export class EthereumProtocolStub implements ProtocolHTTPStub {
   public registerStub(testProtocolSpec: TestProtocolSpec, protocol: EthereumProtocol) {
     sinon
-      .stub(protocol.web3.eth, 'getTransactionCount')
+      .stub(protocol.configuration.nodeClient, 'fetchTransactionCount')
       .withArgs(testProtocolSpec.wallet.addresses[0])
       .returns(Promise.resolve(0))
     sinon
-      .stub(protocol.web3.eth, 'getBalance')
+      .stub(protocol.configuration.nodeClient, 'fetchBalanceOf')
       .withArgs(testProtocolSpec.wallet.addresses[0])
       .returns(Promise.resolve('100000000000000000000'))
   }
