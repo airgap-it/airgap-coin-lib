d07dc649fb545eceb3d3fd88c89c70c66e169682
Mike Godenzi <m.godenzi@papers.ch>
2019-08-30 08:44:13 +0200

    feat(cosmos): added cosmos transaction serialization interfaces

--- src/protocols/cosmos/CosmosProtocol.ts
+++ src/protocols/cosmos/CosmosProtocol.ts
@@ -10,10 +10,115 @@ import * as bip32 from 'bip32'
 import { BIP32Interface } from 'bip32'
 import { NonExtendedProtocol } from '../NonExtendedProtocol'
 
-const secp256k1 = require('secp256k1')
+import axios from 'axios'
+
+// const secp256k1 = require('secp256k1')
 const RIPEMD160 = require('ripemd160')
 
-export class CosmosProtocol extends NonExtendedProtocol implements ICoinProtocol {
+export interface NodeInfo {
+  protocol_version: {
+    p2p: string
+    block: string
+    app: string
+  }
+  id: string
+  listen_addr: string
+  network: string
+  version: string
+  channels: string
+  moniker: string
+  other: {
+    tx_index: string
+    rpc_address: string
+  }
+}
+
+export abstract class CosmosNodeClient {
+  public baseURL: string
+  public identifier: string
+
+  constructor(identifier: string, baseURL: string) {
+    this.baseURL = baseURL
+    this.identifier = identifier
+  }
+
+  public abstract async fetchTransactions(address: string, page: number, limit: number): Promise<IAirGapTransaction[]>
+  public abstract async fetchBalance(address: string): Promise<BigNumber>
+  public abstract async fetchNodeInfo(): Promise<NodeInfo>
+}
+
+export class CosmosJSONRPCNodeClient extends CosmosNodeClient {
+  constructor(identifier: string, baseURL: string = 'https://lcd-do-not-abuse.cosmostation.io') {
+    super(identifier, baseURL)
+  }
+
+  public async fetchTransactions(address: string, page: number, limit: number): Promise<IAirGapTransaction[]> {
+    const promises: Promise<IAirGapTransaction[]>[] = []
+    promises.push(
+      new Promise((resolve, reject) => {
+        axios
+          .get(`${this.baseURL}/txs?message.sender=${address}&page=${page}&limit=${limit}`)
+          .then(response => {
+            const transactionResponse = response.data
+            const airGapTransactions: IAirGapTransaction[] = []
+            for (const transaction of transactionResponse) {
+            }
+            resolve(airGapTransactions)
+          })
+          .catch(reject)
+      })
+    )
+    promises.push(
+      new Promise((resolve, reject) => {
+        axios
+          .get(`${this.baseURL}/txs?transfer.receiver=${address}&page=${page}&limit=${limit}`)
+          .then(response => {
+            const transactionResponse = response.data
+            const airGapTransactions: IAirGapTransaction[] = []
+            for (const transaction of transactionResponse) {
+            }
+            resolve(airGapTransactions)
+          })
+          .catch(reject)
+      })
+    )
+    return Promise.all(promises).then(transactions => {
+      return transactions.reduce((current, next) => {
+        return current.concat(next)
+      })
+    })
+  }
+
+  public async fetchBalance(address: string): Promise<BigNumber> {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(`${this.baseURL}/bank/balances/${address}`)
+        .then(response => {
+          const data: any[] = response.data
+          if (data.length > 0) {
+            resolve(new BigNumber(data[0].amount))
+          } else {
+            resolve(new BigNumber(0))
+          }
+        })
+        .catch(reject)
+    })
+  }
+
+  public async fetchNodeInfo(): Promise<NodeInfo> {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(`${this.baseURL}/node_info`)
+        .then(response => {
+          const nodeInfo = response.data as NodeInfo
+          resolve(nodeInfo)
+        })
+        .catch(reject)
+    })
+  }
+}
+
+export class CosmosProtocol<NodeClient extends CosmosNodeClient> extends NonExtendedProtocol implements ICoinProtocol {
   public symbol: string = '⌀'
   public name: string = 'Cosmos'
   public marketSymbol: string = 'Atom'
@@ -37,17 +142,24 @@ export class CosmosProtocol extends NonExtendedProtocol implements ICoinProtocol
   public addressIsCaseSensitive: boolean = false
   public addressValidationPattern: string = '^cosmos[a-fA-F0-9]{40}$'
   public addressPlaceholder: string = 'cosmosabc...'
-  public blockExplorer: string = ''
+  public blockExplorer: string = 'https://www.mintscan.io'
   public subProtocols?: (ICoinProtocol & ICoinSubProtocol)[] | undefined
+
+  public nodeClient: NodeClient
 
   private addressPrefix = 'cosmos'
 
+  constructor(nodeClient: NodeClient) {
+    super()
+    this.nodeClient = nodeClient
+  }
+
   public getBlockExplorerLinkForAddress(address: string): string {
-    throw new Error('Method not implemented.')
+    return `${this.blockExplorer}/account/${address}`
   }
 
   public getBlockExplorerLinkForTxId(txId: string): string {
-    throw new Error('Method not implemented.')
+    return `${this.blockExplorer}/tx/${txId}`
   }
 
   public getPublicKeyFromHexSecret(secret: string, derivationPath: string): string {
@@ -83,11 +195,23 @@ export class CosmosProtocol extends NonExtendedProtocol implements ICoinProtocol
   }
 
   public async getTransactionsFromPublicKey(publicKey: string, limit: number, offset: number): Promise<IAirGapTransaction[]> {
-    throw new Error('Method not implemented.')
+    return await this.getTransactionsFromAddresses([await this.getAddressFromPublicKey(publicKey)], limit, offset)
+  }
+
+  private getPageNumber(limit: number, offset: number): number {
+    if (limit <= 0 || offset < 0) {
+      return 0
+    }
+    return Math.floor(offset / limit) // we need +1 here because pages start at 1
   }
 
   public async getTransactionsFromAddresses(addresses: string[], limit: number, offset: number): Promise<IAirGapTransaction[]> {
-    throw new Error('Method not implemented.')
+    const promises: Promise<IAirGapTransaction[]>[] = []
+    const page = this.getPageNumber(limit, offset)
+    for (const address in addresses) {
+      promises.push(this.nodeClient.fetchTransactions(address, page, limit))
+    }
+    return Promise.all(promises).then(transactions => transactions.reduce((current, next) => current.concat(next)))
   }
 
   public async signWithPrivateKey(privateKey: Buffer, transaction: any): Promise<string> {
@@ -103,11 +227,19 @@ export class CosmosProtocol extends NonExtendedProtocol implements ICoinProtocol
   }
 
   public async getBalanceOfAddresses(addresses: string[]): Promise<BigNumber> {
-    throw new Error('Method not implemented.')
+    const promises: Promise<BigNumber>[] = []
+    for (const address in addresses) {
+      promises.push(this.nodeClient.fetchBalance(address))
+    }
+    return Promise.all(promises).then(balances => {
+      return balances.reduce((current, next) => {
+        return current.plus(next)
+      })
+    })
   }
 
   public async getBalanceOfPublicKey(publicKey: string): Promise<BigNumber> {
-    throw new Error('Method not implemented.')
+    return await this.getBalanceOfAddresses([await this.getAddressFromPublicKey(publicKey)])
   }
 
   public async prepareTransactionFromPublicKey(
---
+++ src/serializer/unsigned-transactions/cosmos-transactions.serializer.ts
@@ -0,0 +1,98 @@
+import {
+  SerializedSyncProtocolTransaction,
+  SyncProtocolUnsignedTransactionKeys,
+  UnsignedTransaction,
+  UnsignedTransactionSerializer
+} from '../unsigned-transaction.serializer'
+import { toBuffer } from '../utils/toBuffer'
+import BigNumber from 'bignumber.js'
+
+export type SerializedUnsignedCosmosTransaction = [[[Buffer, Buffer, [[Buffer, Buffer]]]], [[[Buffer, Buffer]], Buffer], Buffer]
+
+export interface RawCosmosSendTransaction {
+  messages: RawCosmosMessage[]
+  fee: RawCosmosFee
+  memo: string
+}
+
+export interface RawCosmosMessage {
+  fromAddress: string
+  toAddress: string
+  coins: RawCosmosCoin[]
+}
+
+export interface RawCosmosCoin {
+  denom: string
+  amount: BigNumber
+}
+
+export interface RawCosmosFee {
+  amount: RawCosmosCoin[]
+  gas: BigNumber
+}
+
+export interface UnsignedCosmosTransaction extends UnsignedTransaction {
+  transaction: RawCosmosSendTransaction
+}
+
+export class CosmosTransactionSerializer extends UnsignedTransactionSerializer {
+  public serialize(unsignedTx: UnsignedCosmosTransaction): SerializedSyncProtocolTransaction {
+    const serialized = [
+      [
+        [
+          ...unsignedTx.transaction.messages.map(message => [
+            message.fromAddress,
+            message.toAddress,
+            [
+              ...message.coins.map(coin => {
+                ;[coin.denom, coin.amount]
+              })
+            ]
+          ])
+        ],
+        [[...unsignedTx.transaction.fee.amount.map(amount => [amount.denom, amount.amount])], unsignedTx.transaction.fee.gas],
+        unsignedTx.transaction.memo
+      ],
+      unsignedTx.publicKey, // publicKey
+      unsignedTx.callback ? unsignedTx.callback : 'airgap-wallet://?d=' // callback-scheme
+    ]
+    return toBuffer(serialized) as SerializedSyncProtocolTransaction
+  }
+
+  public deserialize(serializedTx: SerializedSyncProtocolTransaction): UnsignedCosmosTransaction {
+    const cosmosTx = serializedTx[SyncProtocolUnsignedTransactionKeys.UNSIGNED_TRANSACTION] as SerializedUnsignedCosmosTransaction
+    const messages = cosmosTx[0]
+    const fee = cosmosTx[1]
+    const memo = cosmosTx[2]
+
+    const rawCosmosTx: RawCosmosSendTransaction = {
+      messages: messages.map(message => {
+        return {
+          fromAddress: message[0].toString(),
+          toAddress: message[1].toString(),
+          coins: message[2].map(coin => {
+            return {
+              denom: coin[0].toString(),
+              amount: new BigNumber(coin[1].toString())
+            }
+          })
+        }
+      }),
+      fee: {
+        amount: fee[0].map(amount => {
+          return {
+            denom: amount[0].toString(),
+            amount: new BigNumber(amount[1].toString())
+          }
+        }),
+        gas: new BigNumber(fee[1].toString())
+      },
+      memo: memo.toString()
+    }
+    return {
+      transaction: rawCosmosTx,
+      publicKey: serializedTx[SyncProtocolUnsignedTransactionKeys.PUBLIC_KEY].toString(),
+      callback: serializedTx[SyncProtocolUnsignedTransactionKeys.CALLBACK].toString()
+    }
+  }
+}
