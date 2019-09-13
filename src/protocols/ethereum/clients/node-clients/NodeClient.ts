3629769d174202f86ff5cfbf34119c4f604e7e8c
AndreasGassmann <andreas@andreasgassmann.ch>
2019-09-13 16:19:30 +0200

    fix(deps): update all references from merge

--- src/protocols/ethereum/clients/node-clients/NodeClient.ts
+++ src/protocols/ethereum/clients/node-clients/NodeClient.ts
@@ -1,4 +1,4 @@
-import { BigNumber } from 'bignumber.js'
+import { BigNumber } from '../../../../dependencies/src/bignumber.js-9.0.0/bignumber'
 
 export abstract class EthereumNodeClient {
   public baseURL: string
