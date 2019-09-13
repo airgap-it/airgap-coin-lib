f06ade32b7dafa1dd69b47a51110e84e4b388a45
Tim Schluchter <tim.schluchter@uzh.ch>
2019-09-13 11:18:15 +0200

    fix(tezosprotocol): update URL to tezblock.io

--- src/protocols/tezos/TezosProtocol.ts
+++ src/protocols/tezos/TezosProtocol.ts
@@ -124,7 +124,7 @@ export class TezosProtocol extends NonExtendedProtocol implements ICoinProtocol 
   public addressValidationPattern = '^(tz1|KT1)[1-9A-Za-z]{33}$'
   public addressPlaceholder = 'tz1...'
 
-  public blockExplorer = 'https://mvp.tezblock.io'
+  public blockExplorer = 'https://tezblock.io/'
 
   protected readonly transactionFee = new BigNumber('1400')
   protected readonly originationSize = new BigNumber('257')
