a31eb11bffd3f64d2f1aa77ec9e76f713b296d48
Mike Godenzi <m.godenzi@papers.ch>
2019-09-30 11:11:28 +0200

    Revert "chore(cosmos): move file to /actions, renaming to comply with naming convention"
    This reverts commit 786216b6b8f7492e9b8cc2abcfedfd504b06239b.

---
+++ src/actions/CosmosDelegateAction.ts
@@ -0,0 +1,62 @@
+import { CosmosProtocol } from '../../protocols/cosmos/CosmosProtocol'
+import BigNumber from 'bignumber.js'
+import { AirGapMarketWallet } from '../../wallet/AirGapMarketWallet'
+import { IAirGapTransaction } from '../../interfaces/IAirGapTransaction'
+import { RawCosmosTransaction } from '../../serializer/unsigned-transactions/cosmos-transactions.serializer'
+import { Action } from '../../actions/Action'
+import { SyncProtocolUtils, EncodedType } from '../../serializer/serializer'
+
+export interface CosmosDelegationActionContext {
+  wallet: AirGapMarketWallet
+  validatorAddress: string
+  amount: BigNumber
+  undelegate: boolean
+}
+
+export interface CosmosDelegationActionResult {
+  rawTx: RawCosmosTransaction
+  serializedTx: string
+  airGapTxs: IAirGapTransaction[] | void
+  dataUrl: string
+}
+
+export class CosmosDelegateAction<Context extends CosmosDelegationActionContext> extends Action<CosmosDelegationActionResult, Context> {
+  public readonly identifier = 'cosmos-delegate-action'
+
+  protected async perform(): Promise<CosmosDelegationActionResult> {
+    const protocol = new CosmosProtocol()
+    const syncProtocol = new SyncProtocolUtils()
+    const transaction = await protocol.delegate(
+      this.context.wallet.publicKey,
+      this.context.validatorAddress,
+      this.context.amount,
+      this.context.undelegate
+    )
+
+    const serializeTransaction = await syncProtocol.serialize({
+      version: 1,
+      protocol: this.context.wallet.coinProtocol.identifier,
+      type: EncodedType.UNSIGNED_TRANSACTION,
+      payload: {
+        publicKey: this.context.wallet.publicKey,
+        transaction,
+        callback: 'airgap-wallet://?d='
+      }
+    })
+
+    let airGapTransactions: IAirGapTransaction[] | void
+    try {
+      airGapTransactions = await this.context.wallet.coinProtocol.getTransactionDetails({
+        publicKey: this.context.wallet.publicKey,
+        transaction
+      })
+    } catch {}
+
+    return {
+      rawTx: transaction,
+      serializedTx: serializeTransaction,
+      airGapTxs: airGapTransactions,
+      dataUrl: `airgap-vault://?d=${serializeTransaction}`
+    }
+  }
+}
