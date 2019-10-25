cb96d3fabe7da43a6801a90c4e3e63ccf2db544b
Mike Godenzi <m.godenzi@papers.ch>
2019-10-25 16:31:51 +0200

    fix(rewards): improved rewards calculation performance for future cycles

--- examples/delegationRewards.ts
+++ examples/delegationRewards.ts
@@ -4,7 +4,7 @@ const tezosProtocol: TezosProtocol = new TezosProtocol()
 // tz1MJx9vhaNRSimcuXPK2rW4fLccQnDAnVKJ: AirGap
 // tz1eEnQhbwf6trb8Q8mPb2RaPkNk2rN7BKi8
 tezosProtocol
-  .calculateRewards('tz1MJx9vhaNRSimcuXPK2rW4fLccQnDAnVKJ', 160)
+  .calculateRewards('tz1MJx9vhaNRSimcuXPK2rW4fLccQnDAnVKJ', 162)
   .then(result => {
     console.log('REWARDS', result)
     tezosProtocol.calculatePayouts(result, 0, 10).then(result => {
