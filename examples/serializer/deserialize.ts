2ca7bb53f0d92ffa340f685bba31331a8f0279b0
Andreas Gassmann <andreas@andreasgassmann.ch>
2019-11-12 14:16:37 +0100

    chore(): update example

--- examples/serializer/deserialize.ts
+++ examples/serializer/deserialize.ts
@@ -1,15 +1,18 @@
 // tslint:disable:no-console
 
-import { IACMessageDefinitionObject } from '../../src/serializer/v2/message'
-import { Serializer } from '../../src/serializer/v2/serializer.new'
+import { IACMessageDefinitionObject } from '../../src/serializer/message'
+import { Serializer } from '../../src/serializer/serializer'
 
-const url: string =
-  'NDdvRykiZcgZDjNx5APzGRFLiELmSEQpfKEJea8YiRZPFtpL1mNP7UjFmUuqdmrdTxQHGaydKu5ocyYL5G6Ag58ToTXXn5L5aCUtp9FkXHqH5MtLwBy9JmwxW7CeE3F7QS5ZHwofNnBw4XwUw'
+const urls: string[] =
+  ['s3esqztYz6j14zSxv53eKP18NUzkPoD7PGXTZirWKxVKfenfG7BiF4Vz',
+  's3esqzvVjDhPsaGcirpa42nam8QcjxDZrGRiXV8QWtKEU5QPyQVowJrn',
+  's3esqzxSUPmv2Nq3Dv4apqrkTGQMZNF33ALYTzGGjEW949Kt3A8eQfDH',
+  'ekR2AcTWugF7V4tCF4yWjzXj2d']
 
 const serializer: Serializer = new Serializer()
 
 serializer
-  .deserialize([url])
+  .deserialize(urls)
   .then((sync: IACMessageDefinitionObject[]) => {
     console.log('decoded:', sync)
   })
