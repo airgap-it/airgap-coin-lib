d420f70a3bdca47c27309a540ee37379a26a26a3
Mike Godenzi <m.godenzi@papers.ch>
2020-01-29 11:11:30 +0100

    feat(add-baker-payout): added method to calculate a payout for an arbitrary address

--- examples/delegationRewards.ts
+++ examples/delegationRewards.ts
@@ -1,5 +1,6 @@
 import { TezosProtocol } from '../src'
 import { TezosNetwork } from '../src/protocols/tezos/TezosProtocol'
+import BigNumber from '../src/dependencies/src/bignumber.js-9.0.0/bignumber'
 
 const tezosProtocol: TezosProtocol = new TezosProtocol(
   'https://tezos-mainnet-node-1.kubernetes.papers.tech',
@@ -9,11 +10,15 @@ const tezosProtocol: TezosProtocol = new TezosProtocol(
 // tz1MJx9vhaNRSimcuXPK2rW4fLccQnDAnVKJ: AirGap
 // tz1eEnQhbwf6trb8Q8mPb2RaPkNk2rN7BKi8
 tezosProtocol
-  .calculateRewards('tz1MJx9vhaNRSimcuXPK2rW4fLccQnDAnVKJ', 166)
+  .calculateRewards('tz1MJx9vhaNRSimcuXPK2rW4fLccQnDAnVKJ', 167)
   .then(result => {
     console.log('REWARDS', result)
-    tezosProtocol.calculatePayouts(result, 0, 10).then(result => {
-      console.log('PAYOUTS', result)
+    tezosProtocol.calculatePayouts(result, 0, 200).then(result => {
+      // console.log('PAYOUTS', result)
+      console.log('SHARE SUM', result.reduce((current, next) => { return current.plus(new BigNumber(next.share)) }, new BigNumber(0)).toFixed())
+    })
+    tezosProtocol.calculatePayout('tz1MJx9vhaNRSimcuXPK2rW4fLccQnDAnVKJ', result).then(result => {
+      console.log('PAYOUT', result)
     })
   })
   .catch(error => {
