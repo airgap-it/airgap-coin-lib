8469a7cf9016020fee523f3884475e1a209145e7
Mike Godenzi <m.godenzi@papers.ch>
2020-05-06 15:37:37 +0200

    feat(tezos-fa-support-improvements): added staker token

--- src/protocols/tezos/fa/TezosStaker.ts
+++ src/protocols/tezos/fa/TezosStaker.ts
@@ -1,6 +1,32 @@
 import { TezosFAProtocol } from './TezosFAProtocol'
+import { TezosNetwork } from '../TezosProtocol'
 
 export class TezosStaker extends TezosFAProtocol {
 
-    
+    constructor(
+        contractAddress: string = 'KT1EctCuorV2NfVb1XTQgvzJ88MQtWP8cMMv',
+        jsonRPCAPI?: string,
+        baseApiUrl?: string,
+        baseApiKey?: string,
+        baseApiNetwork?: string,
+        network?: TezosNetwork
+      ) {
+        super({
+          symbol: 'STKR',
+          name: 'Staker',
+          marketSymbol: 'stkr',
+          identifier: 'xtz-stkr',
+          feeDefaults: {
+            low: '0.100',
+            medium: '0.300',
+            high: '0.500'
+          },
+          contractAddress: contractAddress,
+          jsonRPCAPI: jsonRPCAPI,
+          baseApiUrl: baseApiUrl,
+          baseApiKey: baseApiKey,
+          baseApiNetwork: baseApiNetwork,
+          network: network
+        })
+      }
 }
