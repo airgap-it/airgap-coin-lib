5b3768615d2217757a63115b87e9da81e7428908
Mike Godenzi <m.godenzi@papers.ch>
2020-06-04 08:32:06 +0200

    chore(adjusted-urls): updated conseil an node urls

--- src/protocols/cosmos/CosmosProtocol.ts
+++ src/protocols/cosmos/CosmosProtocol.ts
@@ -77,7 +77,7 @@ export class CosmosProtocol extends NonExtendedProtocol implements ICoinDelegate
 
   constructor(
     public readonly infoClient: CosmosInfoClient = new CosmosInfoClient(),
-    public readonly nodeClient: CosmosNodeClient = new CosmosNodeClient('https://cosmos-cosmoshub-2.kubernetes.papers.tech', true)
+    public readonly nodeClient: CosmosNodeClient = new CosmosNodeClient('https://cosmos-node.prod.gke.papers.tech')
   ) {
     super()
   }
