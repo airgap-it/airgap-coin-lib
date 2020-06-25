941b0d73a7128dd579bf69fc60f5d17a4f134ede
Mike Godenzi <m.godenzi@papers.ch>
2020-06-25 08:37:33 +0200

    fix(): fixed regression in parsing tzBTC transfers

--- examples/tezosContractCalls.ts
+++ examples/tezosContractCalls.ts
@@ -21,7 +21,7 @@ const contract = new TezosBTC()
 
 // contract.getTransactions(10).then(console.log).catch(console.error)
 
-contract.fetchTokenHolders().then(console.log).catch(console.error)
+// contract.fetchTokenHolders().then(console.log).catch(console.error)
 
 // const body = '{"predicates":[{"field":"parameters_entrypoints","operation":"eq","set":["transfer"],"inverse":true},{"field":"parameters","operation":"isnull","set":[""],"inverse":true},{"field":"kind","operation":"eq","set":["transaction"],"inverse":false},{"field":"destination","operation":"eq","set":["KT1PWx2mnDueood7fEmfbBDKx1D9BAnnXitn"],"inverse":false}],"orderBy":[{"field":"block_level","direction":"desc"}],"limit":100}'
 // Axios.post('https://tezos-mainnet-conseil-1.megan.papers.tech/v2/data/tezos/mainnet/operations', body, { headers: { 'Content-Type': 'application/json', apiKey: 'airgap123' } }).then(response => {
@@ -30,6 +30,8 @@ contract.fetchTokenHolders().then(console.log).catch(console.error)
 //         console.log(normalized.entrypoint, operation.operation_group_hash)
 //     })
 // })
+
+contract.getTransactionsFromAddresses(["tz1Mj7RzPmMAqDUNFBn5t5VbXmWW4cSUAdtT"], 10, 0).then(console.log).catch(console.error)
 
 // contract.getBalance('tz1aqsunnQ9ECPAfvRaWeMfiNFhF3s8M15sy', 'tz1Mj7RzPmMAqDUNFBn5t5VbXmWW4cSUAdtT').then(console.log).catch(console.error)
 
