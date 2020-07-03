1d61db5e405d4016c009101090938c32ee716994
acharl <amadeo@fermat.org>
2020-07-03 18:17:03 +0200

    chore(): switch API to subscan.io

--- src/protocols/substrate/helpers/blockexplorer/SubstrateBlockExplorerClient.ts
+++ src/protocols/substrate/helpers/blockexplorer/SubstrateBlockExplorerClient.ts
@@ -1,33 +1,33 @@
+import { BigNumber } from 'bignumber.js'
 import { IAirGapTransaction } from '../../../../interfaces/IAirGapTransaction'
 import axios from '../../../../dependencies/src/axios-0.19.0'
-import { SubstrateAddress } from '../data/account/SubstrateAddress'
 import { SubstrateNetwork } from '../../SubstrateNetwork'
 
 export class SubstrateBlockExplorerClient {
-    public accountInfoUrl = `${this.baseUrl}/account`
-    public transactionInfoUrl = `${this.baseUrl}/extrinsic`
+  public accountInfoUrl = `${this.baseUrl}/account`
+  public transactionInfoUrl = `${this.baseUrl}/extrinsic`
 
-    constructor(
-        readonly network: SubstrateNetwork,
-        readonly baseUrl: string,
-        readonly apiUrl: string
-    ) {}
+  constructor(readonly network: SubstrateNetwork, readonly baseUrl: string, readonly apiUrl: string, readonly decimals: number) {}
 
-    public async getTransactions(address: string, size: number, pageNumber: number): Promise<Partial<IAirGapTransaction[]>> {
-        const response = await axios.get(`${this.apiUrl}/balances/transfer?&filter[address]=${address}&page[size]=${size}&page[number]=${pageNumber}`)
-        return response.data.data
-            .filter(transfer => transfer.type === 'balancetransfer')
-            .map(transfer => {
-                const destination = SubstrateAddress.fromPublicKey(transfer.attributes.destination.id, this.network).toString()
-                return {
-                    from: [SubstrateAddress.fromPublicKey(transfer.attributes.sender.id, this.network).toString()],
-                    to: [destination],
-                    isInbound: address.includes(destination),
-                    amount: transfer.attributes.value,
-                    fee: transfer.attributes.fee,
-                    hash: transfer.id,
-                    blockHeight: transfer.attributes.block_id
-                }
-            })
-    }
+  public async getTransactions(address: string, size: number, pageNumber: number): Promise<IAirGapTransaction[]> {
+    const body = { row: size, page: pageNumber - 1, address: address }
+    const response = await axios.post(`${this.apiUrl}/transfers`, body)
+    const transfers = response.data.data.transfers
+    return transfers
+      ? transfers
+          .filter((transfer) => transfer.module === 'balances' && transfer.success)
+          .map((transfer) => {
+            return {
+              from: [transfer.from],
+              to: [transfer.to],
+              isInbound: address.includes(transfer.to),
+              amount: new BigNumber(transfer.amount).shiftedBy(this.decimals).toString(),
+              timestamp: transfer.block_timestamp,
+              fee: transfer.fee,
+              hash: transfer.hash,
+              blockHeight: transfer.block_num
+            } as IAirGapTransaction
+          })
+      : []
+  }
 }
--- src/protocols/substrate/implementations/KusamaProtocol.ts
+++ src/protocols/substrate/implementations/KusamaProtocol.ts
@@ -7,56 +7,60 @@ import { SubstrateBlockExplorerClient } from '../helpers/blockexplorer/Substrate
 const NODE_URL = 'https://polkadot-kusama-node.prod.gke.papers.tech'
 
 const BLOCK_EXPLORER_URL = 'https://polkascan.io/pre/kusama'
-const BLOCK_EXPLORER_API = 'https://api-01.polkascan.io/kusama/api/v1'
+const BLOCK_EXPLORER_API = 'https://kusama.subscan.io/api/scan'
+const BLOCK_EXPLORER_DECIMALS = 12
 
 export class KusamaProtocol extends SubstrateProtocol {
-    public symbol: string = 'KSM'
-    public name: string = 'Kusama'
-    public marketSymbol: string = 'KSM'
-    public feeSymbol: string = 'KSM'
+  public symbol: string = 'KSM'
+  public name: string = 'Kusama'
+  public marketSymbol: string = 'KSM'
+  public feeSymbol: string = 'KSM'
 
-    public decimals: number = 12;
-    public feeDecimals: number = 12;
-    public identifier: string = 'kusama';
+  public decimals: number = 12
+  public feeDecimals: number = 12
+  public identifier: string = 'kusama'
 
-    public feeDefaults: FeeDefaults = {
-        low: '0.001', // 1 000 000 000
-        medium: '0.001',
-        high: '0.001'
+  public feeDefaults: FeeDefaults = {
+    low: '0.001', // 1 000 000 000
+    medium: '0.001',
+    high: '0.001'
+  }
+
+  public units: CurrencyUnit[] = [
+    {
+      unitSymbol: 'KSM',
+      factor: '1'
+    },
+    {
+      unitSymbol: 'mKSM',
+      factor: '0.001'
+    },
+    {
+      unitSymbol: 'uKSM',
+      factor: '0.000001'
+    },
+    {
+      unitSymbol: 'Point',
+      factor: '0.000000001'
+    },
+    {
+      unitSymbol: 'Planck',
+      factor: '0.000000000001'
     }
+  ]
 
-    public units: CurrencyUnit[] = [
-        {
-            unitSymbol: 'KSM',
-            factor: '1'
-        },
-        {
-            unitSymbol: 'mKSM',
-            factor: '0.001'
-        },
-        {
-            unitSymbol: 'uKSM',
-            factor: '0.000001'
-        },
-        {
-            unitSymbol: 'Point',
-            factor: '0.000000001'
-        },
-        {
-            unitSymbol: 'Planck',
-            factor: '0.000000000001'
-        }
-    ]
+  public standardDerivationPath: string = `m/44'/434'/0'/0/0` // TODO: verify
 
-    public standardDerivationPath: string = `m/44'/434'/0'/0/0` // TODO: verify
-
-    public constructor(
-        network: SubstrateNetwork = SubstrateNetwork.KUSAMA,
-        nodeClient: SubstrateNodeClient = new SubstrateNodeClient(network, NODE_URL),
-        blockExplorerClient: SubstrateBlockExplorerClient = new SubstrateBlockExplorerClient(
-            network,
-            BLOCK_EXPLORER_URL, 
-            BLOCK_EXPLORER_API
-        ),
-    ) { super(network, nodeClient, blockExplorerClient) }
+  public constructor(
+    network: SubstrateNetwork = SubstrateNetwork.KUSAMA,
+    nodeClient: SubstrateNodeClient = new SubstrateNodeClient(network, NODE_URL),
+    blockExplorerClient: SubstrateBlockExplorerClient = new SubstrateBlockExplorerClient(
+      network,
+      BLOCK_EXPLORER_URL,
+      BLOCK_EXPLORER_API,
+      BLOCK_EXPLORER_DECIMALS
+    )
+  ) {
+    super(network, nodeClient, blockExplorerClient)
+  }
 }
