afd8f4f85776b2833c1f04cd48b8e1821622d89a
acharl <amadeo@fermat.org>
2020-08-24 16:45:05 +0200

    fix(): sort substrate transactions

--- src/protocols/substrate/helpers/blockexplorer/SubstrateBlockExplorerClient.ts
+++ src/protocols/substrate/helpers/blockexplorer/SubstrateBlockExplorerClient.ts
@@ -5,9 +5,14 @@ import { IAirGapTransaction, AirGapTransactionStatus } from '../../../../interfa
 import { SubstrateNetwork } from '../../SubstrateNetwork'
 
 export class SubstrateBlockExplorerClient {
-  constructor(readonly network: SubstrateNetwork, readonly apiUrl: string) { }
+  constructor(readonly network: SubstrateNetwork, readonly apiUrl: string) {}
 
-  public async getTransactions(address: string, limit: number, protocolDecimals: number, cursor?: SubstrateTransactionCursor): Promise<IAirGapTransaction[]> {
+  public async getTransactions(
+    address: string,
+    limit: number,
+    protocolDecimals: number,
+    cursor?: SubstrateTransactionCursor
+  ): Promise<IAirGapTransaction[]> {
     const body = cursor ? { row: limit, page: cursor.page - 1, address: address } : { row: limit, page: 0, address: address }
     const responses = await Promise.all([
       axios.post(`${this.apiUrl}/transfers`, body),
@@ -19,42 +24,42 @@ export class SubstrateBlockExplorerClient {
 
     const airGapTransfers: IAirGapTransaction[] = transfers
       ? transfers
-        .filter((tx) => tx.module === 'balances')
-        .map((tx) => {
-          return {
-            from: [tx.from],
-            to: [tx.to],
-            isInbound: address.includes(tx.to),
-            amount: new BigNumber(tx.amount).shiftedBy(protocolDecimals).toFixed(),
-            timestamp: tx.block_timestamp,
-            fee: tx.fee,
-            hash: tx.hash,
-            blockHeight: tx.block_num,
-            status: tx.success !== undefined
-              ? tx.success ? AirGapTransactionStatus.APPLIED : AirGapTransactionStatus.FAILED
-              : undefined
-          }
-        })
+          .filter((tx) => tx.module === 'balances')
+          .map((tx) => {
+            return {
+              from: [tx.from],
+              to: [tx.to],
+              isInbound: address.includes(tx.to),
+              amount: new BigNumber(tx.amount).shiftedBy(protocolDecimals).toFixed(),
+              timestamp: tx.block_timestamp,
+              fee: tx.fee,
+              hash: tx.hash,
+              blockHeight: tx.block_num,
+              status: tx.success !== undefined ? (tx.success ? AirGapTransactionStatus.APPLIED : AirGapTransactionStatus.FAILED) : undefined
+            }
+          })
       : []
 
     const airGapPayouts: IAirGapTransaction[] = rewardSlash
       ? rewardSlash
-        .filter((tx) => tx.event_id === 'Reward')
-        .map((tx) => {
-          return {
-            from: ['Staking Reward'],
-            to: [address],
-            amount: tx.amount,
-            isInbound: true,
-            timestamp: tx.block_timestamp,
-            fee: '0',
-            hash: tx.extrinsic_hash,
-            blockHeight: tx.block_num,
-            status: AirGapTransactionStatus.APPLIED
-          }
-        })
+          .filter((tx) => tx.event_id === 'Reward')
+          .map((tx) => {
+            return {
+              from: ['Staking Reward'],
+              to: [address],
+              amount: tx.amount,
+              isInbound: true,
+              timestamp: tx.block_timestamp,
+              fee: '0',
+              hash: tx.extrinsic_hash,
+              blockHeight: tx.block_num,
+              status: AirGapTransactionStatus.APPLIED
+            }
+          })
       : []
 
-    return airGapTransfers.concat(airGapPayouts)
+    return airGapTransfers
+      .concat(airGapPayouts)
+      .sort((a, b) => (a.timestamp !== undefined && b.timestamp !== undefined ? b.timestamp - a.timestamp : 0))
   }
 }
