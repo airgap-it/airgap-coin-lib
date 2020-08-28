540e9edd060181dff6e9fc8d32d5af1bfd4301f6
Mike Godenzi <m.godenzi@papers.ch>
2020-08-28 17:50:15 +0200

    fix(): fixed kt test

--- test/protocols/tezos.spec.ts
+++ test/protocols/tezos.spec.ts
@@ -377,7 +377,7 @@ describe(`ICoinProtocol Tezos - Custom Tests`, () => {
       expect(result.airGapTxs[0].amount).to.equal('100000')
       expect(result.airGapTxs[0].fee).to.equal('1420')
       expect(result.rawTezosTx.binaryTransaction).to.equal(
-        'e4b7e31c04d23e3a10ea20e11bd0ebb4bde16f632c1d94779fd5849a34ec42a36c0091a9d2b003f19cf5a1f38f04f1000ab482d33176800fcffe37997800a08d0601ba4e7349ac25dc5eb2df5a43fceacc58963df4f50000'
+        'e4b7e31c04d23e3a10ea20e11bd0ebb4bde16f632c1d94779fd5849a34ec42a36c0091a9d2b003f19cf5a1f38f04f1000ab482d331768c0bcffe37997800a08d0601ba4e7349ac25dc5eb2df5a43fceacc58963df4f50000'
       )
     })
 
