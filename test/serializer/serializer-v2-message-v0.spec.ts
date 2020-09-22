33be3073adbc2d4d31db70eac5d855ee38206ff1
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-09-22 16:36:45 +0000

    fix(test): wrong protocol in tests

--- test/serializer/serializer-v2-message-v0.spec.ts
+++ test/serializer/serializer-v2-message-v0.spec.ts
@@ -12,7 +12,7 @@ const testData = [
   {
     message: {
       type: 4,
-      protocol: 'xtz',
+      protocol: 'ae',
       payload: {
         publicKey: '3874b66ba5d9649082526b39260626796ec2a7364d07cc9bc44b7ca62414166b',
         derivationPath: 'm/44h/457h/0h/0h/0h',
@@ -20,13 +20,13 @@ const testData = [
       }
     },
     result: [
-      '2cervBwKDgab7dYghELbogqDoz6kxk8bCnwVr2cCQeaAUZC73pVWc2Q91cWQjVvG1Xqfakth5xBVSHrokhdb86H6xdtcKQnJjZiCuGVWB5v6XYcUSVXeF1CqHj4pom3sym8uuu5cy3jzX1bdzNo'
+      'NDdvRykiZcgZDjNx5APzGRFLiELmSEQpfKEJea8YiRZPFtpL1mNP14tJoFc6LGxrgmE5v8sTCoBvsgQdhqdjf5wbWnKgXsdZrapdXCkm3yHWuXUcZb5erBVXzC5trquqDE95PBqTfL94zhHyx'
     ]
   },
   {
     message: {
       type: 4,
-      protocol: 'xtz',
+      protocol: 'ae',
       payload: {
         publicKey: '3874b66ba5d9649082526b39260626796ec2a7364d07cc9bc44b7ca62414166b',
         derivationPath: 'm/44h/457h/0h/0h/0h',
@@ -34,14 +34,14 @@ const testData = [
       }
     },
     result: [
-      'FmwWgLpsgrEBoZw7WCZ6JS4MkFFrkEHn4Ajq2QEJEk83qAaqAhw93CHdcTwhK7tAKZBta2hCu6x9nc6eXavNd',
-      'kojrhCXhvkv5exe5pL6oyaZfLdHXVkYV7rTogxrj7B8xuz5fj5gJejrxc3NyErpN7L3vgDd5kEppF3RXfq'
+      'FmwWgLpsgrEBoYsfNnfc1tQkxigCj2GScWYV1xQHTMrHbDWv5TS8fKjgTaoMgjbnHT2G1Ek2vuHXYXgjBgacd',
+      'AtJqRLnpuJdNoqzPMdenWkDVqtYSeJDV1dtskzDA4HEM68XaHNJufi9BehmjqGfPhypf4VKsPiU5izYGP'
     ]
   },
   {
     message: {
       type: 4,
-      protocol: 'xtz',
+      protocol: 'btc',
       payload: {
         publicKey: 'xpub6C8QAa1sHBz38wamg7f1MaF3hrGy9PbASdZ9agpYDYS8DQnYDdhQN458iVQBWpVbCgadtg65E4xLpjPoV2uxTiNdf3PnziySrWnoCauG2PN',
         derivationPath: "m/44'/0'/0'",
@@ -49,13 +49,13 @@ const testData = [
       }
     },
     result: [
-      '5f4YF67Jfi4qfWP7T4Sh5bmyxnd9jSu4yWBU2Ym4vmnHHyDDnccNoBc9hbkQe4oPFr2GUUeyYXYMNm5qQLFbNDNVFhpwGj5C25sNPtPo1hCUM74SqGrTPYo5GmGoirrpC84A35zBPw33XWeFmhB3ff4pdboeNFW4W1qcWJpeBvh87A54HpFahhdUHvXawN129qpYs2t5'
+      '5f4YF67Jfi4qfWP75DhKjyZ1TY2oLR7NcuM4zhAbom6qbvpTJYQQQq6SUiWHYu9WBhXNbUwc1KQuE6mN63JaMqkL7nw9gR2DRn2siCZnuD1wtwXMmeRF4dYVts5NSYgNmXp76ZL9nVV98fBDEMK8xsANTXi43J8DFzRufMmn2XhyqM2tUu4u1wct9m26m2Bfs77TyJ5D'
     ]
   },
   {
     message: {
       type: 4,
-      protocol: 'xtz',
+      protocol: 'btc',
       payload: {
         publicKey: 'xpub6C8QAa1sHBz38wamg7f1MaF3hrGy9PbASdZ9agpYDYS8DQnYDdhQN458iVQBWpVbCgadtg65E4xLpjPoV2uxTiNdf3PnziySrWnoCauG2PN',
         derivationPath: "m/44'/0'/0'",
@@ -63,7 +63,7 @@ const testData = [
       }
     },
     result: [
-      'FmwWgLpsgsoHiTiaqMrEKs5SjwxPoJSg6AHEb5CEbsxW98TfjEsv8YAwAnJW9yEonmdnj4XAZ2NNmYwum52ot',
+      'FmwWgLpsgsoHiTiaqMrEKgUN2z81nQyKZh85nWhyWR5HrQrQbYhS5HGKJ3CSecHoPuKygSB2H39mBhfgVHJYu',
       'FmwWgLpsoou3xySMq2nfg3RczkmsLYtTqwoYHjhKcjvP7C4ijBUGzY9Fn5KahqvfthjdA4YxMbPhbQj2XJtdG',
       'fF3vrSs8swCuXbRLfAGqkJqmB19pLtxDdNJBuMGTqH4hDFfce5KDtCPqRGgoL3qbmyA'
     ]
@@ -71,7 +71,7 @@ const testData = [
   {
     message: {
       type: 4,
-      protocol: 'xtz',
+      protocol: 'cosmos',
       payload: {
         publicKey: '020c4a25d3a5526691c097eff7e9c44bcdd25ba982c7d6704e05eee88e83f61d61',
         derivationPath: "m/44'/118'/0'/0/0",
@@ -79,13 +79,13 @@ const testData = [
       }
     },
     result: [
-      '2cervBwKDgab7dYghELbogqCRjxWSrEw98Sw78o88wHtvAv5n8dibG7kvgVUfrtyRbuYKFoxNvZ3YygXmW5wasPa6RrYcuZqegbggUNUjaiF6nzq8hLM3cfUDg3DSE2GcaP9fKXbHsoDP9WwYzU'
+      '3PrMnoUCzvLG39kMCNkeqLQ7TzPvCqYHd5sQ4Gvuo29WvdogaRnEKuLtegpx89NYnwpjUZJ4uiiW9v2Uo3y7Cq1b5mU49mmo5zdZ4T1PdW3HJHK1X6GKxchLr7CZSbFSG4UznubhvK1HiJPWV3TaPus'
     ]
   },
   {
     message: {
       type: 4,
-      protocol: 'xtz',
+      protocol: 'cosmos',
       payload: {
         publicKey: '020c4a25d3a5526691c097eff7e9c44bcdd25ba982c7d6704e05eee88e83f61d61',
         derivationPath: "m/44'/118'/0'/0/0",
@@ -93,14 +93,15 @@ const testData = [
       }
     },
     result: [
-      'FmwWgLpsgrEBoZw7WCZ6JS4MkFFriJ194qXJMs9NK31sHRi2DdeSXp55QsbVKpGireTndfLwHJw1uKpnGXMRT',
-      'kojrhCXhvkwKRaWYjCYSrMa653res7SL8zSTcireAwRsH4LJrUsfCaTy5s1R8134w6G5ZuLiKfd5Y5tdLJ'
+      'FmwWgLpsgsoHhoaNEXthbvJqh3nYPBkxutbG5dZWxZKSU9Ro6Qf6cATZc1nY3KVq4emZJGmXg12mSGDMVt8K9',
+      'FmwWgLpsoou4Mw2p5npwqBjqcGmfNx96bofgKTkSGUK6x341GzznyzjykHHHuAjNAHNo1hK2FQbG4tE1L2Pgv',
+      'Hg8rBB6q6C6xkj6XMd'
     ]
   },
   {
     message: {
       type: 4,
-      protocol: 'xtz',
+      protocol: 'eth',
       payload: {
         publicKey: '03d79f5ab62e51c7b2960a204e81714826867fa030b1cd74b6ec9931995f130795',
         derivationPath: "m/44'/60'/0'/0/0",
@@ -108,13 +109,13 @@ const testData = [
       }
     },
     result: [
-      'NDdvRykiZcgZDjPKrLxp6vFqBVNePL8ehjwvnW4i5EffB5CAxvvZBih3doZDxopQge2tw4GdCAbnDSvmYCr5H6rFSM9838wM3iPqe5GSveWFgWegfK2DAaocyxEEBvHPi6PVKdfbSfR81XbLd'
+      'NDdvRykiZcgZDjPJNLZW7tA7UtR9vdx8m54Nc67dpJpthN4h4hxG6EHe9wxP68ni3QJWN9QwrtKg6Q8tZDVfUZQzJwzuuo8u4Ux7Zo39HYLDSXzxEM89aNZQtXAU5VBHk3z2wTAvBEvJ4bBz2'
     ]
   },
   {
     message: {
       type: 4,
-      protocol: 'xtz',
+      protocol: 'eth',
       payload: {
         publicKey: '03d79f5ab62e51c7b2960a204e81714826867fa030b1cd74b6ec9931995f130795',
         derivationPath: "m/44'/60'/0'/0/0",
@@ -122,14 +123,14 @@ const testData = [
       }
     },
     result: [
-      'FmwWgLpsgrEBoYsfNnfc4CvZnyhmHcZqKDrpC26hEwKdHffqEG6RTixGVagmKC3KiExhGyH3VqsTGypP3RqD3',
+      'FmwWgLpsgrEBoYsfNnfc43mMsBhDgus2dkF5bL2iiWH8Sgm82M4rfAuf5hdrufn3eyb5Gi36APXHo3MVVnNtD',
       'AtJqRLnpuJdcwtV2HhheusbRVEvAS4ZC7K8GP1wzaN9tpwtDoWakuDzbs6Ueongdz2UAMxB9tqD18hAUN'
     ]
   },
   {
     message: {
       type: 4,
-      protocol: 'xtz',
+      protocol: 'grs',
       payload: {
         publicKey: 'xpub6DFzZR4RHVHG3A16dwMZWYYXTwg5dxerVFtpQFTixfhidujP9XqAeWkGPaH8LhPGpm4Dv3WgpBcrjsqxqhzdr39ZgvTdQzQPjE48u8hxciJ',
         derivationPath: "m/44'/17'/0'",
@@ -137,13 +138,13 @@ const testData = [
       }
     },
     result: [
-      'MZ1hvMj9jMSXcYwZk3iogGHTnPpnT7xya3zDeioK59KYEdpL2VpE1PFSQqXTWm6zbsKv5Gj4KugXmSHw5YwT1A4LzZa5MbcPHDZdzS3bxx3XQApFLAszaqiB2VNuwoK2jYFKfVKaiabGydFjDdjVSSZRbLbNNSfmFcRTvPMsSyCRAmToo4ZBr6Vo2MpxLKMSTLMFrVqNQ'
+      'MZ1hvMj9jMSXcYwYTXLLgmkFqkDKHaDnioPPWeHWB7V6RdeMsE8xc4nhekmuAi65Pa9iZB34He2go3yrdEj8bzT5nccdk2meVno5UdP2CoRHenKp4FTRUBLoST3Pz8RfetTsAxszzf9i6m78kiXcVtbm3qYW6Cb5ujkP1oSwapiGKEqpU8DTTp5r7XBGF9B4TeYzcXwRE'
     ]
   },
   {
     message: {
       type: 4,
-      protocol: 'xtz',
+      protocol: 'grs',
       payload: {
         publicKey: 'xpub6DFzZR4RHVHG3A16dwMZWYYXTwg5dxerVFtpQFTixfhidujP9XqAeWkGPaH8LhPGpm4Dv3WgpBcrjsqxqhzdr39ZgvTdQzQPjE48u8hxciJ',
         derivationPath: "m/44'/17'/0'",
@@ -151,7 +152,7 @@ const testData = [
       }
     },
     result: [
-      'FmwWgLpsgsoHiUn2xmjia6DEhDWVDysyqmwikvqyApzp8NtkNQhKCA4eA6QALHMU9CWpi591kHiWUTDgraJGB',
+      'FmwWgLpsgsoHiUn2xmjiZx1jUSStnyzPJSgRDuEdBjzvaFogArJ2rPczZbWh7WycBPs68TGy1ssk255adi9Af',
       'FmwWgLpsoou6E9V8nXR5SdS8Tkt2jFBdJN5AfJJgP18WE3VjRZdyqQBY15mnwzYGB18pe6qm92PyrNfYWHznt',
       '3vWTx4y3CqmzK18JsyqjhhgZz9983LrjN35QWyVqQx54h7x7wogUNmD9P9tHW8zCrShMr'
     ]
@@ -159,7 +160,7 @@ const testData = [
   {
     message: {
       type: 4,
-      protocol: 'xtz',
+      protocol: 'polkadot',
       payload: {
         publicKey: '62af6bc3692698152918c618c9e9f995972ac299654d404998d6ec8055f6272c',
         derivationPath: "m/44'/354'/0'/0/0",
@@ -167,13 +168,13 @@ const testData = [
       }
     },
     result: [
-      '5op5c6ufhBzafCMERhTpzNT6NTq3Rxx2vFiH2AAEzJjMDw3YuDZqVBb4L7hVTmcqDit77nYsy8JGTNHPp63cRGYL15StPpzDL84zA7JWBC5HbPmNwgvuRfg3YH7DhBfjGUnc4S7EcuCYZMXA'
+      '3PrMnoUCzvLG39kRpVYvHtPSPaKM4VzPSncu9KNEB5SoAFYLg5urNqe4Vs174SPoiqW9mfyxWTKKiUqdv9pt8WwnMf4WNNtRXW9fk4M28F9KuBqMa4Ef7MGA42WcijtX3dM5zJ9f2HysSNSab4Pbvqy'
     ]
   },
   {
     message: {
       type: 4,
-      protocol: 'xtz',
+      protocol: 'polkadot',
       payload: {
         publicKey: '62af6bc3692698152918c618c9e9f995972ac299654d404998d6ec8055f6272c',
         derivationPath: "m/44'/354'/0'/0/0",
@@ -181,14 +182,15 @@ const testData = [
       }
     },
     result: [
-      'FmwWgLpsgrEBoXpDFNn7oynmqi9ftsRAYwNgLJaGSqeixozXawwZGGhgjtKyhk7kxTD6T7c9vnEV12ZdxJYZv',
-      '3EN264ymrRziDC5oZpNo1K44XnzpXkWuYyy39KNiq2QHZxKYHiiSbstFR3QshcvkYvKS8QbMyL4V4xbE'
+      'FmwWgLpsgsoHhoaNEXthgHNg9w2gxTjLoAns92xsaijPziTHUMtdSdTjdHj5dEV9af96Ba2xaxUKFCjsYsB1F',
+      'FmwWgLpsoou4bzo3SHb4uxDRij2HKk4Yi9JURsoxEHVCBZM2f9ZBkCnvDiMDTaPRHNNs5LGtp3wNVCkaCoCMZ',
+      'Hg8rBB6q6C54uqjH2b'
     ]
   },
   {
     message: {
       type: 4,
-      protocol: 'xtz',
+      protocol: 'kusama',
       payload: {
         publicKey: 'dc17d447b04b542c082177e9e499db2f474a41e772e43f5f1fd3a8fc1e3b332a',
         derivationPath: "m/44'/434'/0'/0/0",
@@ -196,13 +198,13 @@ const testData = [
       }
     },
     result: [
-      '5op5c6ufhBzafCMERhTpzNT6NTq3Rxx2vTvqnXYUoBsbTiFQDQvb6A7Ya912mHWkVigNUeWia8Bfi7up56sXo27ezenuiC6qudNFRZKfYJTR5Ga15mMXgfVgUHAkqUU9UuZ4KUsYNBpKZ6Yb'
+      '88NVC6aU2DpZxXp9AM7pz1P3B2ZDsTNPUC9BMz7MCea5xMbv9dpbWhsEnJyZ1G5zpdUZyFb8SWU4BBh37G6ozydP4JzuwG2jWofDXxpCzvKxHemCEn7roRNMqhrYDcRBm7kvTN8hNZxwst7NG5Mz'
     ]
   },
   {
     message: {
       type: 4,
-      protocol: 'xtz',
+      protocol: 'kusama',
       payload: {
         publicKey: 'dc17d447b04b542c082177e9e499db2f474a41e772e43f5f1fd3a8fc1e3b332a',
         derivationPath: "m/44'/434'/0'/0/0",
@@ -210,8 +212,8 @@ const testData = [
       }
     },
     result: [
-      'FmwWgLpsgrEBoXpDFNn7oynmqi9ftsRAYwNgLJg1V166c4QXAcqRH91dwo8F3FuA777PLYFbs1sqFzsKyt9Fq',
-      '3EN264ymrRzf4wWSwzqKLNREgfH3EXYwpRK9VhCSE4UQLnrdNcz1yoVrasTK9mnxCN6zs6x336QiQHj5'
+      'FmwWgLpsgrEBoazZdcSaew8PNUwNxjJBpRsxwaRAQYRngHzZvKDjQG5jDakoy8mBzCDmwbEWgcHHGxaqCvFTZ',
+      'FmwWgLpsonKx9sJewbNKfD1EZPDomAQG78bzNexF2KCGcoVikAS79S9L6SXRWigRzt76hxnsKPs4srBxsQQSW'
     ]
   },
   {
