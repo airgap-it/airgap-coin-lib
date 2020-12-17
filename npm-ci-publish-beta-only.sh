086677c041bf605e71b0f7f7fde89fd169edf206
Julia Samól <j.samol@papers.ch>
2020-12-17 15:29:57 +0100

    fix(release): ignore changes in package.json before publish

--- npm-ci-publish-beta-only.sh
+++ npm-ci-publish-beta-only.sh
@@ -4,6 +4,9 @@ echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > .npmrc
 git update-index --assume-unchanged npm-ci-publish.sh
 git update-index --assume-unchanged npm-ci-publish-beta-only.sh
 
+git update-index --assume-unchanged package.json
+git update-index --assume-unchanged package-lock.json
+
 VERSION=$(node -pe 'JSON.parse(process.argv[1]).version.indexOf("beta")' "$(cat lerna.json)")
 
 if [ "$VERSION" = "-1" ]
