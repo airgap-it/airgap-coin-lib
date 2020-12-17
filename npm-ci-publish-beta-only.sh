e24ed6f9571aee4ea20f60b2eeceb82724eb6f50
Julia Samól <j.samol@papers.ch>
2020-12-17 10:04:13 +0100

    refactor(core): move sources to packages

--- npm-ci-publish-beta-only.sh
+++ npm-ci-publish-beta-only.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > .npmrc
 
+git update-index --assume-unchanged npm-ci-publish.sh
+git update-index --assume-unchanged npm-ci-publish-beta-only.sh
+
 VERSION=$(node -pe 'JSON.parse(process.argv[1]).version.indexOf("beta")' "$(cat package.json)")
 
 if [ "$VERSION" = "-1" ]
@@ -8,7 +11,7 @@ then
   echo "cannot publish non-beta version"
 else
   echo "version is beta, using --tag next"
-  npm publish --tag next
+  npx lerna publish from-package --contents dist --dist-tag next --yes
 fi
 
 rm .npmrc
--- npm-ci-publish.sh
+++ npm-ci-publish.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > .npmrc
 
+git update-index --assume-unchanged npm-ci-publish.sh
+git update-index --assume-unchanged npm-ci-publish-beta-only.sh
+
 VERSION=$(node -pe 'JSON.parse(process.argv[1]).version.indexOf("beta")' "$(cat package.json)")
 
 if [ "$VERSION" = "-1" ]
 then
-  npm publish
+  npx lerna publish from-package --contents dist --yes
 else
   echo "version is beta, using --tag next"
-  npm publish --tag next
+  npx lerna publish from-package --contents dist --dist-tag next --yes
 fi
 
 rm .npmrc
