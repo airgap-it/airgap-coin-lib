1c7b45a45db0ba16d0e4baa4bd6094d511f90b97
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-12-18 16:19:44 +0000

    fix(tezos): message signing

--- src/protocols/tezos/TezosCryptoClient.ts
+++ src/protocols/tezos/TezosCryptoClient.ts
@@ -3,6 +3,17 @@ import * as sodium from 'libsodium-wrappers'
 import * as bs58check from '../../dependencies/src/bs58check-2.1.2'
 import { Ed25519CryptoClient } from '../Ed25519CryptoClient'
 
+function isHex(str: string): boolean {
+  const legend = '0123456789abcdef';
+  for (let i = 0; i < str.length; i++) {
+    if (legend.includes(str[i])) {
+      continue;
+    };
+    return false;
+  };
+  return true;
+};
+
 export class TezosCryptoClient extends Ed25519CryptoClient {
   constructor(public readonly edsigPrefix: Uint8Array = new Uint8Array([9, 245, 205, 134, 18])) {
     super()
@@ -11,7 +22,9 @@ export class TezosCryptoClient extends Ed25519CryptoClient {
   public async signMessage(message: string, keypair: { privateKey: Buffer }): Promise<string> {
     await sodium.ready
 
-    const hash: Buffer = sodium.crypto_generichash(32, sodium.from_string(message))
+    const bufferMessage = await this.toBuffer(message)
+
+    const hash: Buffer = sodium.crypto_generichash(32, bufferMessage)
     const rawSignature: Uint8Array = sodium.crypto_sign_detached(hash, keypair.privateKey)
     const signature: string = bs58check.encode(Buffer.concat([Buffer.from(this.edsigPrefix), Buffer.from(rawSignature)]))
 
@@ -31,9 +44,18 @@ export class TezosCryptoClient extends Ed25519CryptoClient {
       throw new Error(`invalid signature: ${signature}`)
     }
 
-    const hash: Buffer = sodium.crypto_generichash(32, sodium.from_string(message))
+    const bufferMessage = await this.toBuffer(message)
+
+    const hash: Buffer = sodium.crypto_generichash(32, bufferMessage)
     const isValidSignature: boolean = sodium.crypto_sign_verify_detached(rawSignature, hash, Buffer.from(publicKey, 'hex'))
 
     return isValidSignature
+  }
+
+  private async toBuffer(message: string): Promise<Buffer> {
+    return isHex(message) ? // Check if the string is a hex string
+      Buffer.from(message, 'hex') // If yes, then we interpret it as hex
+      : sodium.from_string(message) // If not, then we let sodium convert it to a buffer (this is not compatible with the tezos-cli)
+
   }
 }
