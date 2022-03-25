d080ceb47bb0fcafcb78dd1ba278dfcf50f8735f
Mike Godenzi <m.godenzi@papers.ch>
2022-03-25 14:41:08 +0100

    Feat/ithaca

--- packages/core/src/protocols/substrate/astar/AstarProtocolOptions.ts
+++ packages/core/src/protocols/substrate/astar/AstarProtocolOptions.ts
@@ -15,7 +15,7 @@ import {
 
 const MAINNET_NAME: string = 'Mainnet'
 
-const NODE_URL: string = 'https://astar-proxy.airgap.prod.gke.papers.tech'
+const NODE_URL: string = 'https://astar-node.prod.gke.papers.tech'
 
 const BLOCK_EXPLORER_URL: string = 'https://astar.subscan.io'
 const BLOCK_EXPLORER_API: string = 'https://astar.subscan.prod.gke.papers.tech/api/scan'
